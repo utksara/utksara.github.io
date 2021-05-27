fb4cb3090dba8c4d5abffa0d12ff2eba0a2e7dc2
Utkarsh Saraswat <Utkarsh>
2021-05-27 18:14:41 +0530

    made the cell alive

--- cellmech/src/code.js
+++ cellmech/src/code.js
@@ -15,14 +15,19 @@ var center = {
     "y" : Math.floor(screen.height/2)
 }
 
-function create_fluctuations(magnitude, no_of_points){
-    const A = Math.random() * magnitude;
-    const B = Math.random() * magnitude;
-    const C = Math.random() * magnitude;
+function create_fluctuations(magnitude, no_of_points, phase = Math.PI/2){
+    // const A = Math.random() * magnitude;
+    // const B = Math.random() * magnitude;
+    // const C = Math.random() * magnitude;
+
+    random_fluct = 0.1*(Math.random() -0.5)
+    const A = 0.33 * magnitude;
+    const B = 0.33 * magnitude;
+    const C = 0.33 * magnitude;
     const f = 2;
     points = []
     for (var theta = 0; theta < 2 * Math.PI; theta += 2 * Math.PI/no_of_points){
-        var r = (A*Math.sin(f * theta) + B*Math.sin( 2 * theta + Math.PI/2) + C*Math.sin( 3 * theta + 3*Math.PI/2));
+        var r = (A*Math.sin(f * theta) + B*Math.sin( 2 * theta + phase) + C*Math.sin( 3 * theta));
         points.push([r*Math.cos(theta), r*Math.sin(theta)]);
     }
     return points;
@@ -64,6 +69,35 @@ var shape = add_coords(circ, fluct)
 points = tranform_coordinates_to_string(shape)
 
 document.getElementById("hom").setAttribute('points', points);
-document.getElementById("canvas").setAttribute('height',screen.height)
-document.getElementById("canvas").setAttribute('width',screen.width)
+document.getElementById("canvas").setAttribute('height',screen.height);
+document.getElementById("canvas").setAttribute('width',screen.width);
 
+var time = 0;
+var i = 0;
+
+function move(starting_point){
+    time  = time +1;
+    i = time/10;
+    foint = tranform_coordinates_to_string([[(starting_point + i)%1000, 500], [(starting_point + 100 + i)%1000, 500], [(starting_point + i)%1000, 600]])
+    document.getElementById("unf").setAttribute('points',foint);
+}
+
+function alive(){
+    time  = time +1;
+    // i = i + 2*(Math.random()-0.5)/100;
+    i = i + 1/100;
+    var points;
+
+    var cell_membrane = create_circle(100, 100);
+    var cell_membrane_fluct = create_fluctuations(20, 100, i%(2*Math.PI));
+    points = tranform_coordinates_to_string(add_coords(cell_membrane, cell_membrane_fluct))
+    document.getElementById("hom").setAttribute('points',points);
+
+    var nucleus = create_circle(30, 100);
+    var nucleus_fluct = create_fluctuations(7, 100, (i/2)%(2*Math.PI));
+    points = tranform_coordinates_to_string(add_coords(nucleus, nucleus_fluct))
+    document.getElementById("unf").setAttribute('points',points);
+
+}
+
+setInterval( alive, 1);
